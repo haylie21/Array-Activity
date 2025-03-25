2f00f029ee521f1997f35cf59bf983c7f04f1249
haylie21 <haylie.antolin08@gmail.com>
2025-03-26 01:02:31 +0800

    Update arrayActivity.js

--- arrayActivity.js
+++ arrayActivity.js
@@ -1,17 +1,17 @@
-// Create an array called favSingers and store your three favorite singers in it.  
-const favSingers = ["Singer1", "Singer2", "Singer3"];  
+// Create an array called favSingers and store your three favorite singers in it.
+const favSingers = ["ABBA", "Laufey", "Carpenters"];
 
-// Log the first singer in the array using array index notation.  
-console.log("First singer in the array:", favSingers[0]);  
+// Log the first singer in the array using array index notation.
+console.log("First singer in the array:", favSingers[0]);
 
-// Create an array called favNumbers and store four of your favorite numbers.  
-const favNumbers = [7, 14, 21, 28];  
+// Create an array called favNumbers and store four of your favorite numbers.
+const favNumbers = [2, 8, 13, 21];
 
-// Create an array named mixedArr and store different data types.  
-const mixedArr = ["string", ["otherarray"], 123, true];  
+// Create an array named mixedArr and store different data types.
+const mixedArr = ["music", ["nestedArray"], 2024, false];
 
-// Access and log each item in the mixedArr using array index notation.  
-console.log("First item in mixedArr:", mixedArr[0]);  
-console.log("Second item in mixedArr:", mixedArr[1]);  
-console.log("Third item in mixedArr:", mixedArr[2]);  
-console.log("Fourth item in mixedArr:", mixedArr[3]);  
+// Access and log each item in the mixedArr using array index notation.
+console.log("First item in mixedArr:", mixedArr[0]);
+console.log("Second item in mixedArr:", mixedArr[1]);
+console.log("Third item in mixedArr:", mixedArr[2]);
+console.log("Fourth item in mixedArr:", mixedArr[3]);
